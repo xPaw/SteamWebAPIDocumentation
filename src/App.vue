6b41e515d3c663b3bdb8a6d17b06d07734ec30a2
Pavel Djundik <xPaw@users.noreply.github.com>
2023-12-03 23:38:08 +0200

    Simpler instruction to get access token

--- src/App.vue
+++ src/App.vue
@@ -135,15 +135,11 @@
 								<p>Some APIs work with OAuth access token, if you have one you can provide it here and it will be preferred over the webapi key.</p>
 								<p>Here's how to get one:</p>
 								<ul>
-									<li>Open <a href="https://store.steampowered.com/points/shop/" target="_blank" rel="noopener">https://store.steampowered.com/points/shop/</a></li>
-									<li>Run the following script:</li>
+									<li>Open <a href="https://store.steampowered.com/pointssummary/ajaxgetasyncconfig" target="_blank" rel="noopener"><code>https://store.steampowered.com/pointssummary/ajaxgetasyncconfig</code></a></li>
+									<li>Copy the value of <code>webapi_token</code></li>
 								</ul>
-								<div class="input-group mb-3">
-									<button class="btn btn-outline-primary pt-0" type="button" @click.prevent="copyUrl($event)">
-										<svg viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-label="Copy"><path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path></svg>
-									</button>
-									<div class="form-control font-monospace h-auto">javascript:JSON.parse(application_config.dataset.loyaltystore).webapi_token</div>
-								</div>
+
+								<p>You can then use <a href="#ISteamUserOAuth/GetTokenDetails">ISteamUserOAuth/GetTokenDetails</a> endpoint to get information about the token.</p>
 							</div>
 						</div>
 
